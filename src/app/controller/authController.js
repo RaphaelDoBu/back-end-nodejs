931b4eaf59f94a883d123dc3be4414e427d4bf23
Jose Raphael <jose.bu@ccc.ufcg.edu.br>
2020-06-15 14:15:47 -0300

    retirada de console

--- src/app/controller/authController.js
+++ src/app/controller/authController.js
@@ -16,8 +16,7 @@ function generateToken(params = {}){
 
 router.post('/register', async(req, res) =>{
     const {email} = req.body;
-
-    console.log({email})
+    
     try{
         if(await User.findOne({email})){
             return res.status(400).send({error: 'User already exists'});
