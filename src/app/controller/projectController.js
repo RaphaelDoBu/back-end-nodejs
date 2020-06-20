4f6844be0ea87d1d0826febe43558281c134ee3e
Jose Raphael <jose.bu@ccc.ufcg.edu.br>
2020-06-20 11:27:31 -0300

    alteracao do controller de project com novas rotas

--- src/app/controller/projectController.js
+++ src/app/controller/projectController.js
@@ -5,8 +5,23 @@ const authMiddleware = require('../middlewares/auth');
 router.use(authMiddleware);
 
 router.get('', (req, res) => {
-    res.send({ok: true})
+    res.send({user: req.userId})
 });
 
+router.get('/:projectId', async(req, res) => {
+    res.send({user: req.userId})
+});
+
+router.post('/', async(req, res) => {
+    res.send({ok: true, user: req.userId})
+});
+
+router.put('/:projectId', async(req, res) => {
+    res.send({user: req.userId})
+});
+
+router.delete('/:projectId', async(req, res) => {
+    res.send({user: req.userId})
+});
 
 module.exports = app => app.use('/projects', router);
